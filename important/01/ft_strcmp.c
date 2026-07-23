312e2adf3ea3e49f59a2e3d55152b30b7799774a
Mubaraq Dauda <88740848+Dauda77@users.noreply.github.com>
2026-07-23 17:22:57 +0200

    Refactor ft_strcmp to use index-based comparison

--- important/01/ft_strcmp.c
+++ important/01/ft_strcmp.c
@@ -2,10 +2,10 @@
 
 int   ft_strcmp(char *s1, char *s2)
 {
-    while (*s1 && *s1 == *s2)
+    int i = 0
+    while (s1[i] && s1[i] == s2[i])
     {
-        s1++;
-        s2++;
+        i++;
     }
-    return (*s1 - *s2);
+    return (s1[i] - s2[i]);
 }
