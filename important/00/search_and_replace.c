9496666a1bffe777a3e33d3903df86a9ef0b8985
Mubaraq Dauda <88740848+Dauda77@users.noreply.github.com>
2026-07-23 15:51:31 +0200

    Increment av[1] after writing to output

--- important/00/search_and_replace.c
+++ important/00/search_and_replace.c
@@ -10,7 +10,7 @@ int main(int ac, char **av)
                     write(1, av[3], 1);
                 else
                     write(1, av[1], 1);
-                av[1];
+                av[1]++;
             }
     }
     write(1, "\n", 1);
