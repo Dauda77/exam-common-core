fe79b96cc5763e62b5e84d1950c94bc625f6392b
Mubaraq Dauda <88740848+Dauda77@users.noreply.github.com>
2026-07-23 14:13:53 +0200

    Simplify string matching in wdmatch.c
    Refactor wdmatch.c to simplify string matching logic.

--- important/01/wdmatch.c
+++ important/01/wdmatch.c
@@ -2,28 +2,23 @@
 
 int	main(int ac, char **av)
 {
-	int i = 0;
-	int j = 0;
-	char *str1 = av[1];
-	char *str2 = av[2];
+
+	char *tmp;
+	
 	if (ac == 3)
 	{
-		while (str2[j])
+		tmp = av[1];
+		while (*tmp && *av[2])
 		{
-			if (str1[i] == str2[j])
-				i++;
-			j++;
+			while (*tmp == *av[2] && (*tmp && *av[2]))
+			{
+				tmp++;
+				av[2]++;
+			}
+			av[2]++;
 		}
-		if (str1[i] == '\0')
-		{
-            int k = 0;
-            while (str1[k])
-            {
-                write(1, &str1[k], 1);
-                k++;
-            }
-            
-		}
+		if (!(*tmp))
+			ft_putstr(av[1]);
 	}
 	write(1, "\n", 1);
 }
